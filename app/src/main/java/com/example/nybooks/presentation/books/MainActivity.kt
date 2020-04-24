93ee482634d3f1c173557351dcea64c6e132918e
JefferssonSemin <jeffesemin@hotmail.com>
2020-04-24 13:52:09 -0300

    Adicionado o recyclerView e listado objetos

--- app/src/main/java/com/example/nybooks/presentation/books/MainActivity.kt
+++ app/src/main/java/com/example/nybooks/presentation/books/MainActivity.kt
@@ -2,7 +2,10 @@ package com.example.nybooks.presentation.books
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.nybooks.R
+import com.example.nybooks.data.model.Book
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -11,8 +14,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        toolbarMain.title = "Books"
+        toolbarMain.title = getString(R.string.books_title)
         setSupportActionBar(toolbarMain)
+
+        with(recyclerBooks){
+            layoutManager = LinearLayoutManager(this@MainActivity, RecyclerView.VERTICAL, false )
+            setHasFixedSize(true)
+            adapter = BooksAdapter(getBooks())
+        }
+    }
+
+    fun getBooks(): List<Book>{
+        return listOf(
+            Book("O poder do hábito", "Gary taubes")
+
+        )
     }
 
 }
